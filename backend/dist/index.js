e30b7878767fd8c8bc3fbc27ac856d47995b23e5
ssprasad-cyber <2040320ssprasad990@gmail.com>
2024-06-08 23:59:03 +0530

    backend

---
+++ backend/dist/index.js
@@ -0,0 +1,8 @@
+import app from './app.js';
+import { connectToDatabase } from './db/connection.js';
+// connections and listeners
+const PORT = process.env.PORT || 5000;
+connectToDatabase().then(() => {
+    app.listen(PORT, () => console.log("Server open and connected to database 🤙"));
+}).catch((err) => console.log(err));
+//# sourceMappingURL=index.js.map
